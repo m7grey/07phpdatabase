c9d90ae45554446da71b6620604397bcc5fd272f
m7grey <m7grey@gmail.com>
2017-03-25 14:00:58 -0400

    sadfgh

--- employeeList.php
+++ employeeList.php
@@ -1,16 +1,17 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <!--    filename employee.php-->
+    <!--    filename employeeList.php-->
     <meta charset="UTF-8">
     <title>Page Title </title>
     <link rel="stylesheet" type="text/css" href="them.css">
 </head>
 <body>
+<h1>Display all Employee</h1>
 <?php for ($i = 1; $i < 100; $i++) { ?>
 <div class="row">
-    <span class="col1">aaa</span>
-    <span class="col2">bbb</span>
+    <span class="col1">lastName, firstName</span>
+    <span class="col2">salary</span>
 </div>
 <?php } ?>
 
