daf8f801450e89172eb02be49f517a9b38cfec4c
m7grey <m7grey@gmail.com>
2017-03-25 13:27:51 -0400

    print function

--- employeeList.php
+++ employeeList.php
@@ -1,17 +1,18 @@
 <!doctype html>
 <html lang="en">
 <head>
-<!--    filename employee.php-->
+    <!--    filename employee.php-->
     <meta charset="UTF-8">
     <title>Page Title </title>
     <link rel="stylesheet" type="text/css" href="them.css">
 </head>
 <body>
-
+<?php for ($i = 1; $i < 100; $i++) { ?>
 <div class="row">
     <span class="col1">aaa</span>
     <span class="col2">bbb</span>
 </div>
+<?php } ?>
 
 </body>
 </html>
