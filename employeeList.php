1684e1dae80feb96b421b2df176833273aa12bbc
m7grey <m7grey@gmail.com>
2017-03-25 13:20:45 -0400

    Class week 8. initial start

---
+++ employeeList.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+<!--    filename employee.php-->
+    <meta charset="UTF-8">
+    <title>Page Title </title>
+    <link rel="stylesheet" type="text/css" href="them.css">
+</head>
+<body>
+
+<div class="row">
+    <span class="col1">aaa</span>
+    <span class="col2">bbb</span>
+</div>
+
+</body>
+</html>
